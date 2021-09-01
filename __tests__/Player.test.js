39dcd1bfd2e96aa5ee21bfb7d9686a88a2564679
abraham perez reyes <abrahampre27@gmail.com>
2021-08-31 19:43:44 -0500

    10.3.3

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,4 @@
 
-
 const { test, expect } = require('@jest/globals');
 const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion.js');
@@ -41,4 +40,32 @@ test('gets inventory from player or returns false', ()=>{
     expect(player.getInventory()).toEqual(false);
 });
 
+test('get players health value',()=> {
+    const player = new Player ('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player ('Dave');
+  
+    expect(player.isAlive()).toBeTruthy();
+  
+    player.health = 0;
+  
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from players health", () => {
+    const player = new Player ('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth -5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
 
